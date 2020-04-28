3ef8583806133792260a683a583356f2f9725419
Brs <jancicboris56@gmail.com>
2020-04-28 18:20:03 +0100

    Main folder created

---
+++ src/main/loadUsers.java
@@ -0,0 +1,22 @@
+package main;
+
+import models.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class loadUsers {
+
+    public void load() throws FileNotFoundException {
+        File file = new File("src/login/korisnici.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+
+
+
+    }
+
+}
