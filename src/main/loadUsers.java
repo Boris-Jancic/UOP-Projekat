3fbcd4a18a7ccb7413f4e1d3eb31293167ec02bd
Brs <jancicboris56@gmail.com>
2020-04-28 19:46:37 +0100

    Implemented loadUsers module

--- src/main/loadUsers.java
+++ src/main/loadUsers.java
@@ -2,21 +2,45 @@ package main;
 
 import models.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 
 public class loadUsers {
 
-    public void load() throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+        load();
+    }
+
+    public static void load() throws IOException {
         File file = new File("src/login/korisnici.txt");
         BufferedReader reader = new BufferedReader(new FileReader(file));
 
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] userSplit = line.split("\\|");
+            String name = userSplit[0];
+            String lastName = userSplit[1];
+            String jmbg = userSplit[2];
+            String gender = userSplit[3];
+            String addres = userSplit[4];
+            String phone = userSplit[5];
+            String username = userSplit[6];
+            String password = userSplit[7];
+            String id = userSplit[8];
+            String role = userSplit[9];
 
-
-
+            if (role.equals("1")) {
+                client c = new client(id, name, lastName, jmbg, gender, phone, username, password, id);
+                System.out.println(c.toString());
+            }
+            if (role.equals("2")) {
+                worker w = new worker(id, name, lastName, jmbg, gender, phone, username, password, id);
+                System.out.println(w.toString());
+            }
+            if (role.equals("3")) {
+                admin a = new admin(id, name, lastName, jmbg, gender, phone, username, password, id);
+                System.out.println(a.toString());
+            }
+        }
     }
-
 }
