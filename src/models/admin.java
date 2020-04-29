05d00c28d895436941f13e3d85a9b827e8ccecbb
Brs <jancicboris56@gmail.com>
2020-04-29 20:28:54 +0100

    Changed instance variables for all classes

--- src/models/admin.java
+++ src/models/admin.java
@@ -2,8 +2,10 @@ package models;
 
 public class admin extends employe {
 
-    public admin(String id, String name, String lastName, String jmbg, String gender, String address, String phone, String username, String password) {
-        super(id, name, lastName, jmbg, gender, address, phone, username, password);
+
+    public admin(String id, String name, String lastName, String jmbg, String gender
+            , String address, String phone, String username, String password, double sallary) {
+        super(id, name, lastName, jmbg, gender, address, phone, username, password, sallary);
     }
 
     private void addMembers(){
--- src/models/worker.java
+++ src/models/worker.java
@@ -3,10 +3,29 @@ package models;
 public class worker extends employe {
     private String specialization;
 
-    public worker(String id, String name, String lastName, String jmbg, String gender, String address, String phone, String username, String password) {
-        super(id, name, lastName, jmbg, gender, address, phone, username, password);
+
+    public worker(String id, String name, String lastName, String jmbg,
+                  String gender, String address, String phone, String username,
+                  String password, String specialization, Double sallary) {
+        super(id, name, lastName, jmbg, gender, address, phone, username, password, sallary);
+        this.specialization = specialization;
     }
 
+    @Override
+    public String toString() {
+        return "worker{" +
+                "specialization='" + specialization + '\'' +
+                ", id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", jmbg='" + jmbg + '\'' +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 
     public String getSpecialization() {
         return specialization;
