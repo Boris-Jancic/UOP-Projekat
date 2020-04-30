e9935d3a051931164df21ca6495f5c52b1d19ab6
Brs <jancicboris56@gmail.com>
2020-04-30 20:25:57 +0100

    Enums are now in a seperate file

---
+++ src/carModels/Enums.java
@@ -0,0 +1,17 @@
+package carModels;
+
+enum status{
+    DONE,FINISHED,NOTFINISHED
+}
+
+enum mark{
+    BMW,LAMBO,YUGO,OPEL
+}
+
+enum fuel{
+    DIZEL,ELECTRIC,GAS
+}
+
+enum model{
+    ONE,TWO,THREE,FOUR
+}
