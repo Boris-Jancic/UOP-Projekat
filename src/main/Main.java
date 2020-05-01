ad9dfd7a9f03c14b5ef0e4c6793dd63935d82631
Brs <jancicboris56@gmail.com>
2020-05-01 15:25:13 +0100

    Optimized IO

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,32 +9,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         RegisterUser rU = new RegisterUser();
         LoadUsers lU = new LoadUsers();
-        RegisterCar rC= new RegisterCar();
+        RegisterCar rC = new RegisterCar();
         LoadCars lC = new LoadCars();
 
-        System.out.println("1) Registruj korisnika");
-        System.out.println("2) Ucitaj korisnike");
-        System.out.println("3) Registruj automobil");
-        System.out.println("4) Ucitaj automobile");
+        String option = "";
+        while (option != "0") {
+            System.out.println("\n1) Registruj korisnika");
+            System.out.println("2) Ucitaj korisnike");
+            System.out.println("3) Registruj automobil");
+            System.out.println("4) Ucitaj automobile");
+            System.out.println("0) Ugasi aplikaciju");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(">>> Unesi funkciju : ");
-        int option = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(">>> Unesi funkciju : ");
+            option = scanner.nextLine();
 
-        if (option == 1){
-            rU.register();
+            if (option.equals("1")) {
+                rU.register();
+            }
+            if (option.equals("2")) {
+                lU.load();
+            }
+            if (option.equals("3")) {
+                rC.register();
+            }
+            if (option.equals("4")) {
+                lC.load();
+            }
+            if (option.equals("0")) {
+                break;
+            }
         }
-        if (option == 2){
-            lU.load();
-        }
-        if (option == 3){
-            rC.register();
-        }
-        if (option == 4){
-            lC.load();
-        }
-
-
     }
-
 }
