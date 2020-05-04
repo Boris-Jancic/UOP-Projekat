65fd3d878da8e4c470412b88cb8c7c7130411e54
Brs <jancicboris56@gmail.com>
2020-05-04 21:44:32 +0100

    New constructor that includes ID

--- src/userModels/Client.java
+++ src/userModels/Client.java
@@ -1,8 +1,10 @@
 package userModels;
 
+import carModels.Car;
+
 public class Client extends Person {
     private int points;
-    //private Car[] cars;
+    private Car[] cars;
 
     public Client(String name, String lastName, String jmbg, String gender,
                   String address, String phone, String username, String password, int points) {
@@ -10,11 +12,27 @@ public class Client extends Person {
         this.points = points;
     }
 
+    public Client(String name, String lastName, String jmbg, String gender,
+                  String address, String phone, String username, String password,
+                  int points, String id) {
+        super(name, lastName, jmbg, gender, address, phone, username, password);
+        this.points = points;
+        this.id = id;
+    }
+
+    public Car[] getCars() {
+        return cars;
+    }
+
+    public void setCars(Car[] cars) {
+        this.cars = cars;
+    }
 
     @Override
     public String toString() {
         return "client{" +
                 "points=" + points +
+                ", cars='" + this.cars + '\'' +
                 ", id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 ", lastName='" + lastName + '\'' +
