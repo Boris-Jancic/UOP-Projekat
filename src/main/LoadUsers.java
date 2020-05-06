b770c8df9d3016c198c8cc435bd2392d2687facc
Brs <jancicboris56@gmail.com>
2020-05-06 21:26:37 +0100

    Load now returns a list of Person objects and takes Cars as paramaters

--- src/main/LoadUsers.java
+++ src/main/LoadUsers.java
@@ -1,17 +1,20 @@
 package main;
 
+import carModels.Car;
 import userModels.*;
 import utility.Checks;
 import utility.ReadFromFile;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class LoadUsers {
 
-    public void load() throws IOException {
+    public ArrayList<Person> load(ArrayList<Car> Cars) throws IOException {
         ReadFromFile readFromFile = new ReadFromFile();
         Checks c = new Checks();
         String[] users = readFromFile.read("src/data/korisnici.txt").split("\n");
+        ArrayList<Person> people = new ArrayList<Person>();
 
         for (String user : users){
             String[] userSplit = user.split("\\|");
@@ -27,25 +30,25 @@ public class LoadUsers {
             String id = userSplit[9];
 
             if (role.equals("1")) {
-                int points = Integer.parseInt(userSplit[10]);
                 Client client = c.findClient(id);
                 client.setId(id);
-                System.out.println(client.toString());
+                client.setCars(c.findCars(Cars, id));
+                people.add(client);
             }
             if (role.equals("2")) {
                 Double sallary = Double.parseDouble(userSplit[10]);
                 String specialization = userSplit[11];
                 Worker w = new Worker(name, lastName, jmbg, gender, address, phone, username, password, specialization, sallary);
                 w.setId(id);
-                System.out.println(w.toString());
+                people.add(w);
             }
             if (role.equals("3")) {
                 Double sallary = Double.parseDouble(userSplit[10]);
                 Admin a = new Admin(name, lastName, jmbg, gender, address, phone, username, password, sallary);
                 a.setId(id);
-                System.out.println(a.toString());
+                people.add(a);
             }
-            // TODO: zastita ako nema role
         }
+        return people;
     }
 }
