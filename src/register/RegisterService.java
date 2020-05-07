40b9944d93bdd84dcdc47a57dc04e4e349ae93f2
Brs <jancicboris56@gmail.com>
2020-05-07 16:21:44 +0100

    Services can register dates,cars and workers

--- src/register/RegisterService.java
+++ src/register/RegisterService.java
@@ -9,16 +9,25 @@ import java.util.Scanner;
 
 public class RegisterService {
 
-    public void register(){
+    public void register() {
         PickEnums pickEnums = new PickEnums();
         Scanner scanner = new Scanner(System.in);
         Checks check = new Checks();
         WriteToFile tofile = new WriteToFile();
 
+        System.out.print("\n>>> ID radnika :");
+        String workerID = scanner.nextLine();
+
+        if (check.findWorker(workerID) == null) {
+            System.out.println("\n! Nema datog radnika !");
+            return;
+        }
+
         System.out.print(">>> ID automobila za servisiranje :");
         String carID = scanner.nextLine();
 
         if (!check.ifCarExists(carID)) {
+            System.out.println("\n! Nema datog automobila !");
             return;
         }
 
@@ -28,17 +37,27 @@ public class RegisterService {
         System.out.print(">>> Opis :");
         String description = scanner.nextLine();
 
-        System.out.print(">>> Iskorisceni delovi :");
+
+        System.out.print(">>> Iskorisceni delovi (odvojeni zarezom => ID,ID) :");
         String usedParts = scanner.nextLine();
+
+        if (check.findParts(usedParts.split(",")) == null) {
+            System.out.println("Nema datih delova");
+            return;
+        }
 
         String status = pickEnums.pickStatus();
 
         Random r = new Random();
         int rand = r.nextInt(999999);
 
-        String service = carID + "|" + date + "|" + description  + "|" + usedParts
-                + "|" + status  + "|" + rand;
-        System.out.println(service);
+        String service = carID + "|" + workerID + "|" + date + "|" + description
+                + "|" + usedParts + "|" + status  + "|" + rand;
+
+        String carBook = carID + "|" + rand;
+
+        if(!check.writeCarBook(carID, Integer.toString(rand)))
+            tofile.write(carBook, "src/data/carbooks.txt");
 
         tofile.write(service,"src/data/services.txt");
     }
