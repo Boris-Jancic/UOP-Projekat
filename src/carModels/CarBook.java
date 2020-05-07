9edf2ffd1b0b0e1ea9bb2da769a7e1606997e01d
Brs <jancicboris56@gmail.com>
2020-05-07 16:18:08 +0100

    Added toString and getters/setters for services

--- src/carModels/CarBook.java
+++ src/carModels/CarBook.java
@@ -6,9 +6,32 @@ public class CarBook {
     private Car car;
     private ArrayList<Service> services;
 
-    public CarBook(carModels.Car car) {
+    public CarBook(Car car) {
         this.car = car;
     }
+
+    @Override
+    public String toString() {
+        return "CarBook{" +
+                "car=" + car.getCarID() +
+                ", services=" + printParts() +
+                '}';
+    }
+
+    private String printParts(){
+        String output = "";
+
+        for (Service service : services){
+            output += "|" + service.getId() + "|";
+
+        }
+        return output;
+    }
+
+
+    public void setServices(ArrayList<Service> services) { this.services = services; }
+
+    public ArrayList<Service> getServices() { return services; }
 
     public carModels.Car getCar() { return car; }
 
