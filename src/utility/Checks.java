f30bc60d2e3126f0a7fc267609a69ee5f53d3ecd
Brs <jancicboris56@gmail.com>
2020-05-14 19:10:03 +0100

    Fixed null bug and role error

--- src/utility/Checks.java
+++ src/utility/Checks.java
@@ -18,10 +18,11 @@ import java.util.GregorianCalendar;
 public class Checks {
 
     public ArrayList<Car> findCars(ArrayList<Car> Cars, String id) {
-        ArrayList<Car> carsReturn = new ArrayList<Car>();
+        ArrayList<Car> carsReturn = new ArrayList<>();
 
         for (Car car : Cars) {
-            if (car.getClient().getId().equals(id)) {
+            Client client = car.getClient();
+            if (client.getId().equals(id)) {
                 carsReturn.add(car);
             }
         }
@@ -30,101 +31,72 @@ public class Checks {
 
 
     public Car findCar(ArrayList<Car> Cars, String carID) {
-        Car returnCar = null;
         for (Car car : Cars) {
             if (car.getCarID().equals(carID)) {
-                returnCar = car;
+                return car;
             }
         }
-        return returnCar;
-    }
-
-    public Boolean ifCarExists(String id) {
-        ReadFromFile readFromFile = new ReadFromFile();
-        String[] cars = ReadFromFile.read("src/data/cars.txt").split("\n");
-
-        for (String car : cars) {
-            String[] userSplit = car.split("\\|");
-            String carID = userSplit[1];
-
-            if (carID.equals(id)) {
-                return true;
-            }
-        }
-        return false;
+        return null;
     }
 
     public Worker findWorker(String id) {
         ReadFromFile readFromFile = new ReadFromFile();
         String[] users = readFromFile.read("src/data/korisnici.txt").split("\n");
 
-        for (String user : users) {
-            String[] userSplit = user.split("\\|");
-            String role = userSplit[0];
-            String userID = userSplit[9];
-            String name = userSplit[1];
-            String lastName = userSplit[2];
-            String jmbg = userSplit[3];
-            String gender = userSplit[4];
-            String address = userSplit[5];
-            String phone = userSplit[6];
-            String username = userSplit[7];
-            String password = userSplit[8];
+        if (users[0] != "") {
+            for (String user : users) {
+                String[] userSplit = user.split("\\|");
+                String role = userSplit[0];
+                String userID = userSplit[9];
+                String name = userSplit[1];
+                String lastName = userSplit[2];
+                String jmbg = userSplit[3];
+                String gender = userSplit[4];
+                String address = userSplit[5];
+                String phone = userSplit[6];
+                String username = userSplit[7];
+                String password = userSplit[8];
 
-            if (role.equals("2") && userID.equals(id)) {
-                Double sallary = Double.parseDouble(userSplit[10]);
-                String specialization = userSplit[11];
-                Worker w = new Worker(name, lastName, jmbg, gender, address, phone,
-                        username, password, specialization, sallary);
-                w.setId(userID);
-                return w;
+                if (role.equals("2") && userID.equals(id)) {
+                    Double sallary = Double.parseDouble(userSplit[10]);
+                    String specialization = userSplit[11];
+                    Worker w = new Worker(name, lastName, jmbg, gender, address, phone,
+                            username, password, specialization, sallary, id);
+                    w.setId(userID);
+                    return w;
+                }
             }
         }
         return null;
     }
 
     public Client findClient(String id) {
-        ReadFromFile readFromFile = new ReadFromFile();
         String[] users = ReadFromFile.read("src/data/korisnici.txt").split("\n");
 
-        for (String user : users) {
-            String[] userSplit = user.split("\\|");
-            String role = userSplit[0];
-            String userID = userSplit[9];
-            String name = userSplit[1];
-            String lastName = userSplit[2];
-            String jmbg = userSplit[3];
-            String gender = userSplit[4];
-            String address = userSplit[5];
-            String phone = userSplit[6];
-            String username = userSplit[7];
-            String password = userSplit[8];
+        if (!users[0].equals("")) {
+            for (String user : users) {
+                String[] userSplit = user.split("\\|");
+                String role = userSplit[0];
+                String name = userSplit[1];
+                String lastName = userSplit[2];
+                String jmbg = userSplit[3];
+                String gender = userSplit[4];
+                String address = userSplit[5];
+                String phone = userSplit[6];
+                String username = userSplit[7];
+                String password = userSplit[8];
+                String userID = userSplit[9];
 
-            if (role.equals("1") && userID.equals(id)) {
-                String points = userSplit[10];
+                if (role.equals("3") && userID.equals(id)) {
+                    String points = userSplit[10];
+                    Client client = new Client(name, lastName, jmbg, gender, address, phone,
+                            username, password, Integer.parseInt(points), userID);
 
-                return new Client(name, lastName, jmbg, gender, address, phone,
-                        username, password, Integer.parseInt(points), userID);
+                    return client;
+                }
             }
         }
         return null;
-    }
-
-    public Boolean ifUsersExists(String id, String roleP) {
-        ReadFromFile readFromFile = new ReadFromFile();
-        String[] users = ReadFromFile.read("src/data/korisnici.txt").split("\n");
-
-        for (String user : users) {
-            String[] userSplit = user.split("\\|");
-            String role = userSplit[0];
-            String userID = userSplit[9];
-
-            if (role.equals(roleP) && userID.equals(id)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     public ArrayList<Part> findParts(String[] PartIDs) {
@@ -157,26 +129,28 @@ public class Checks {
 
 
     public Boolean writeCarBook(String carIDP, String newServiceID) {
-        ReadFromFile readFromFile = new ReadFromFile();
 
         String[] carBooks = ReadFromFile.read("src/data/carbooks.txt").split("\n");
         String output = "";
-        boolean b = false;
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/data/carbooks.txt"))) {
-            for (String carBook : carBooks) {
-                String[] carBookSplit = carBook.split("\\|");
-                System.out.println(Arrays.toString(carBookSplit));
-                String carID = carBookSplit[0];
-                String serviceIDs = carBookSplit[1];
 
-                if (carIDP.equals(carID)) {
-                    carBook = carBook.replace(serviceIDs, serviceIDs + "," + newServiceID);
-                    b = true;
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/data/carbooks.txt"))) {
+            if (!carBooks[0].equals("")) {
+                for (String carBook : carBooks) {
+                    String[] carBookSplit = carBook.split("\\|");
+                    System.out.println(Arrays.toString(carBookSplit));
+                    String carID = carBookSplit[0];
+                    String serviceIDs = carBookSplit[1];
+
+                    if (carIDP.equals(carID)) {
+                        carBook = carBook.replace(serviceIDs, serviceIDs + "," + newServiceID);
+
+                    }
+                    output += carBook + "\n";
                 }
-                output += carBook + "\n";
+                writer.write(output);
+                return true;
             }
-            writer.write(output);
-            return b;
+
         } catch (IOException e) {
             System.out.println("Fajl ne postoji");
         }
