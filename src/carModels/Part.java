cbd214b5b4ecd6d899956c6c8bd84dd3610d9e10
Brs <jancicboris56@gmail.com>
2020-05-14 19:04:26 +0100

    Deleted unnecessary id

--- src/carModels/Part.java
+++ src/carModels/Part.java
@@ -1,7 +1,5 @@
 package carModels;
 
-import java.util.Random;
-
 public class Part {
     private String mark;
     private String model;
@@ -9,16 +7,13 @@ public class Part {
     private double price;
     private String id;
 
-    final Random r = new Random();
-    final int rand = r.nextInt(999999);
-
     public Part(String mark, String model, String name,
-                double price) {
+                double price, String id) {
         this.mark = mark;
         this.model = model;
         this.name = name;
         this.price = price;
-        this.id = Integer.toString(rand);
+        this.id = id;
     }
 
     @Override
