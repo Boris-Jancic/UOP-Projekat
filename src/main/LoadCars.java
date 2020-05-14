17dd3006087f1f216c5375c2d0f8068e4594305a
Brs <jancicboris56@gmail.com>
2020-05-14 19:07:55 +0100

    Added check for empty file

--- src/main/LoadCars.java
+++ src/main/LoadCars.java
@@ -12,31 +12,32 @@ public class LoadCars {
 
     public ArrayList<Car> load() {
         Checks c = new Checks();
-        ReadFromFile readFromFile = new ReadFromFile();
         String[] cars = ReadFromFile.read("src/data/cars.txt").split("\n");
-        ArrayList<Car> Cars = new ArrayList<Car>();
+        ArrayList<Car> Cars = new ArrayList<>();
 
-        for (String car : cars) {
-            String[] carSplit = car.split("\\|");
-            String clientId = carSplit[0];
-            Client client = c.findClient(clientId);
+        if (cars[0] != "") {
+            for (String car : cars) {
+                String[] carSplit = car.split("\\|");
+                String clientId = carSplit[0];
 
-            String carId = carSplit[1];
-            String mark = carSplit[2];
-            String model = carSplit[3];
-            String fuel = carSplit[4];
-            String age = carSplit[5];
-            String engineVolume = carSplit[6];
-            String enginePower = carSplit[7];
+                Client client = c.findClient(clientId);
 
-            Car Car = new Car(client, carId, mark, model, fuel, age,
-                    Float.parseFloat(engineVolume), Integer.parseInt(enginePower));
+                String carId = carSplit[1];
+                String mark = carSplit[2];
+                String model = carSplit[3];
+                String fuel = carSplit[4];
+                String age = carSplit[5];
+                String engineVolume = carSplit[6];
+                String enginePower = carSplit[7];
 
+                Car Car = new Car(client, carId, mark, model, fuel, age,
+                        Float.parseFloat(engineVolume), Integer.parseInt(enginePower));
 
-            CarBook carBook = new CarBook(Car);
+                CarBook carBook = new CarBook(Car);
 
-            Car.setCarBook(carBook);
-            Cars.add(Car);
+                Car.setCarBook(carBook);
+                Cars.add(Car);
+            }
         }
         return Cars;
     }
