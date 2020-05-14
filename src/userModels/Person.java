4ffe03d85131abee1a1fd1f3d0d1fc1343be402c
Brs <jancicboris56@gmail.com>
2020-05-14 19:11:35 +0100

    Deleted unnecessary id

--- src/userModels/Person.java
+++ src/userModels/Person.java
@@ -1,7 +1,5 @@
 package userModels;
 
-import java.util.Random;
-
 public abstract class Person {
     protected String id;
     protected String name;
@@ -12,11 +10,8 @@ public abstract class Person {
     protected String phone;
     protected String username;
     protected String password;
-    final Random r = new Random();
-    final int rand = r.nextInt(999999);
 
     public Person(String name, String lastName, String jmbg, String gender, String address, String phone, String username, String password) {
-        this.id = Integer.toString(rand);
         this.name = name;
         this.lastName = lastName;
         this.jmbg = jmbg;
