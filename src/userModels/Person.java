0c59772673ae4d14b9b059e4eda97b31e1e022c3
Boris Jancic <jancicboris56@gmail.com>
2020-05-28 23:35:08 +0200

    Added getters and setters

--- src/userModels/Person.java
+++ src/userModels/Person.java
@@ -22,12 +22,44 @@ public abstract class Person {
         this.password = password;
     }
 
-
     public String personToString(){
         return this.name + "|" + this.lastName + "|" + this.jmbg + "|" + this.gender + "|" +
                 this.address + "|" + this.phone + "|" + this.username + "|" + this.password + "|" + this.id;
     }
 
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getJmbg() { return jmbg; }
+
+    public void setJmbg(String jmbg) { this.jmbg = jmbg; }
+
+    public String getGender() { return gender; }
+
+    public void setGender(String gender) { this.gender = gender; }
+
+    public String getAddress() { return address; }
+
+    public void setAddress(String address) { this.address = address; }
+
+    public String getPhone() { return phone; }
+
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
     public String getId() { return this.id; }
+
     public void setId(String id) { this.id = id; }
 }
