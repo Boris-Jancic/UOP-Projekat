c9734149c224b1338eec3d8fce679207daae8ffc
Boris Jancic <jancicboris56@gmail.com>
2020-05-28 23:40:38 +0200

    Load method now takes cars and improved general outcome

--- src/main/LoadCarBooks.java
+++ src/main/LoadCarBooks.java
@@ -1,39 +1,43 @@
 package main;
 
+import carModels.Car;
 import carModels.CarBook;
 import carModels.Service;
+import utility.Checks;
 import utility.ReadFromFile;
 
 import java.util.ArrayList;
 
 public class LoadCarBooks {
-    public static ArrayList<CarBook> load(ArrayList<Service> services) {
+    public static ArrayList<CarBook> load(ArrayList<Car> cars, ArrayList<Service> services) {
         ArrayList<CarBook> returnCarBooks = new ArrayList<>();
 
         String[] carBooks = ReadFromFile.read("src/data/carbooks.txt").split("\n");
 
-        if (carBooks[0] != "") {
-            for (String carBook : carBooks) {
+        for (String carBook : carBooks) {
+            if (!carBook.isEmpty()) {
                 ArrayList<Service> carBookServices = new ArrayList<>();
 
                 String[] carBookSplit = carBook.split("\\|");
-                String carBookCar = carBookSplit[0];
+                Car carBookCar = Checks.findCar(carBookSplit[0], cars);
+                String[] serviceIDs = carBookSplit[1].split(";");
 
-                CarBook c = null;
+                CarBook c = new CarBook(carBookCar);
 
-                    for (Service service : services) {
-                        if (service.getCar().getCarID().equals(carBookCar)) {
-                            c = new CarBook(service.getCar());
-                            carBookServices.add(service);
-                        }
+                for (String serviceID : serviceIDs) {
+                    Service service = Checks.findService(serviceID, services);
+
+                    if (service != null) {
+                        carBookServices.add(service);
                     }
-
-                        c.setServices(carBookServices);
-                        returnCarBooks.add(c);
                 }
-            }
 
-            return returnCarBooks;
+                c.setServices(carBookServices);
+                returnCarBooks.add(c);
+            }
+        }
+
+        return returnCarBooks;
     }
 }
 
