580c9befa9aa1ae1207860478a4250e368df487a
Boris Jancic <jancicboris56@gmail.com>
2020-05-28 23:39:03 +0200

    Load method now takes parts and delimiter for parts changed , -> ;

--- src/main/LoadServices.java
+++ src/main/LoadServices.java
@@ -13,19 +13,19 @@ import java.util.GregorianCalendar;
 import java.util.Set;
 
 public class LoadServices {
-    public static ArrayList<Service> load(ArrayList<Car> cars, Set<Person> people) {
+    public static ArrayList<Service> load(ArrayList<Car> cars, Set<Person> people, ArrayList<Part> parts) {
         String[] servicesString = ReadFromFile.read("src/data/services.txt").split("\n");
         ArrayList<Service> Services = new ArrayList<>();
 
-        if (servicesString[0] != "") {
-            for (String service : servicesString) {
+        for (String service : servicesString) {
+            if (!service.isEmpty()) {
                 String[] serviceSplit = service.split("\\|");
 
-                Car car = Checks.findCar(cars, serviceSplit[0]);
+                Car car = Checks.findCar(serviceSplit[0], cars);
                 Worker worker = Checks.findWorker(serviceSplit[1], people);
                 GregorianCalendar date = Checks.stringToDate(serviceSplit[2]);
                 String description = serviceSplit[3];
-                ArrayList<Part> usedParts = Checks.findParts(serviceSplit[4].split(","));
+                ArrayList<Part> usedParts = Checks.findParts(serviceSplit[4].split(";"), parts);
                 String serviceID = serviceSplit[6];
                 String state = serviceSplit[5];
 
