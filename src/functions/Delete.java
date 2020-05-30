fa627df5212e15930ab4f68d1d19f821b7838f10
Boris Jancic <jancicboris56@gmail.com>
2020-05-30 02:28:14 +0200

    New methods added

--- src/functions/Delete.java
+++ src/functions/Delete.java
@@ -9,77 +9,110 @@ import userModels.Client;
 import userModels.Person;
 import userModels.Worker;
 import utility.Checks;
+
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
  public class Delete {
 
-    public static Set<Person> deletePerson(Set<Person> people) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n1) Izbrisi admina");
-        System.out.println("2) Izbrisi radnika");
-        System.out.println("3) Izbrisi musteriju");
-
-        System.out.print("\n>>> Unesi funkciju : ");
-        String option = scanner.nextLine();
-        System.out.print("\n>>> Unesite ID korisnika kojeg zelite da obrisete : ");
-        String deletedPerson = scanner.nextLine();
-
+    public static Person deletePerson(String option, String deletedPerson, Set<Person> people) {
         for (Person person: people) {
             if (person != null){
                 if (option.equals("1") && person instanceof Admin && deletedPerson.equals(person.getId())){
-                    people.remove(person);
-                    return people;
+                    return person;
                 }
 
                 if (option.equals("2") && person instanceof Worker && deletedPerson.equals(person.getId())){
-                    people.remove(person);
-                    return people;
+                    return person;
                 }
 
                 if (option.equals("3") && person instanceof Client && deletedPerson.equals(person.getId())){
-                    people.remove(person);
-                    return people;
+                    return person;
                 }
             }
         }
 
         System.out.println("\n! Takav korisnik ne postoji ! \n");
-        return people;
+        return null;
     }
 
 
-    public static ArrayList<Car> deleteCar(String carID, ArrayList<Car> cars) {
-        Car deletedCar = Checks.findCar(carID, cars);
-        cars.remove(deletedCar);
+    public static Car deleteCar(String carID, ArrayList<Car> cars) {
+        return Checks.findCar(carID, cars);
+    }
+
+    public static ArrayList<Car> deleteCars(ArrayList<Car> clientCars, ArrayList<Car> cars){
+        Iterator carsIterator = cars.iterator();
+
+        while (carsIterator.hasNext()) {
+            Car iteratorCar = (Car) carsIterator.next();
+
+            for (Car clientCar : clientCars) {
+                if (iteratorCar.getCarID().equals(clientCar.getCarID())) {
+                    carsIterator.remove();
+                }
+            }
+        }
+
         return cars;
     }
+
 
     public static ArrayList<Part> deletePart(ArrayList<Part> parts) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("\n>>> Unesite ID dela koji zelite da obrisete : ");
         String partID = scanner.nextLine();
 
-        Part deletedPart = Checks.findPart(partID, parts);
-        parts.remove(deletedPart);
+        for (Part part : parts) {
+            if (part.getId().equals(partID)){
+                part.setAvailable(false);
+            }
+        }
+
         return parts;
     }
 
 
-    public static ArrayList<CarBook> deleteCarBook(String carID, ArrayList<CarBook> carBooks) {
-        CarBook deletedCarBook = Checks.findCarBook(carID, carBooks);
-        carBooks.remove(deletedCarBook);
-        return carBooks;
-    }
-
-     public static ArrayList<Service> deleteService(ArrayList<Service> services) {
+     public static Service deleteService(ArrayList<Service> services) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("\n>>> Unesite ID dela koji zelite da obrisete : ");
         String serviceID = scanner.nextLine();
 
         Service deletedService = Checks.findService(serviceID, services);
-        services.remove(deletedService);
+        return deletedService;
+     }
+
+     public static ArrayList<Service> deleteServices(String workerID, ArrayList<Service> services) {
+        Iterator serviceIterator = services.iterator();
+
+        while (serviceIterator.hasNext()) {
+            Service service = (Service) serviceIterator.next();
+            if (workerID.equals(service.getWorker().getId())) {
+                serviceIterator.remove();
+            }
+        }
+
         return services;
      }
-}
+
+
+     public static ArrayList<CarBook> deleteCarBook(String carID, ArrayList<CarBook> carBooks) {
+         CarBook deletedCarBook = Checks.findCarBook(carID, carBooks);
+         carBooks.remove(deletedCarBook);
+         return carBooks;
+     }
+
+
+     public static ArrayList<CarBook> deleteCarBookServices(Service service, ArrayList<CarBook> carBooks) {
+        for (CarBook carBook : carBooks) {
+            if (carBook.getServices().contains(service)){
+                carBook.removeService(service);
+            }
+        }
+
+        return carBooks;
+     }
+ }
