bec33e3499f557a1c89cdafaf4c7569fcd8de572
Boris Jancic <jancicboris56@gmail.com>
2020-06-04 18:32:58 +0200

    Added input checks & updated car constructor

--- src/register/RegisterCar.java
+++ src/register/RegisterCar.java
@@ -6,6 +6,7 @@ import utility.Checks;
 import utility.PickEnums;
 import utility.WriteToFile;
 
+import java.util.Calendar;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -19,7 +20,7 @@ public class RegisterCar {
         String clientID = scanner.nextLine();
         Client client = Checks.findClient(clientID);
 
-        if (client != null) {
+        if (client != null || client.isDeleted() == true) {
             Random r = new Random();
             int rand = r.nextInt(999999);
 
@@ -31,13 +32,29 @@ public class RegisterCar {
             System.out.print(">>> Godiste (Samo godina proizvodnje): ");
             String age = scanner.nextLine();
 
+            int year = Calendar.getInstance().get(Calendar.YEAR);
+            if (!age.matches("[0-9]+") || age.length() != 4 || Integer.parseInt(age) > year) {  // Proverava da li je
+                System.out.println("!!! Pogresno uneseno godiste !!!");                               // godina validna
+                return null;
+            }
+
             System.out.print(">>> Zapremina motora : ");
+            if (!scanner.hasNextFloat()) {
+                System.out.println("!!! Unesite broj !!!");
+                return null;
+            }
             String engineVolume = scanner.nextLine();
 
+
             System.out.print(">>> Jacina motora : ");
+            if (!scanner.hasNextInt()) {
+                System.out.println("!!! Unesite broj !!!");
+                return null;
+            }
             String enginePower = scanner.nextLine();
 
-            Car car = new Car(client, Integer.toString(rand), mark, model, fuel, age, Float.parseFloat(engineVolume), Integer.parseInt(enginePower));
+            Car car = new Car(client, Integer.toString(rand), mark, model, fuel, age,
+                    Float.parseFloat(engineVolume), Integer.parseInt(enginePower), false);
             return car;
         }
         System.out.println("\n! Korisnik sa takvim ID-om ne postoji !");
