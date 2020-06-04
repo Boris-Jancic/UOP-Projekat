4d5ccb8b9680d1e294524712716d357b67d04063
Boris Jancic <jancicboris56@gmail.com>
2020-06-04 18:18:41 +0200

    Now picks enums till the right option is entered

--- src/utility/PickEnums.java
+++ src/utility/PickEnums.java
@@ -7,140 +7,144 @@ public class PickEnums {
     private static Scanner scanner = new Scanner(System.in);
 
     public static String pickMark() {
-        System.out.println("\n1) Bmw");
-        System.out.println("2) Lambo");
-        System.out.println("3) Yugo");
-        System.out.println("4) Opel");
-        System.out.print(">>> Marka : ");
-        String option = scanner.nextLine();
-
+        String option = "";
         String m = "";
 
-        if (option.equals("1")) {
-            m = mark.BMW.toString();
-        }
-        if (option.equals("2")) {
-            m = mark.LAMBO.toString();
-        }
-        if (option.equals("3")) {
-            m = mark.YUGO.toString();
-        }
-        if (option.equals("4")) {
-            m = mark.OPEL.toString();
+        while (!option.matches("[1-4]")) {
+            System.out.println("\n1) Bmw");
+            System.out.println("2) Lambo");
+            System.out.println("3) Yugo");
+            System.out.println("4) Opel");
+            System.out.print(">>> Marka : ");
+            option = scanner.nextLine();
+
+            if (option.equals("1")) {
+                m = mark.BMW.toString();
+            } else if (option.equals("2")) {
+                m = mark.LAMBO.toString();
+            } else if (option.equals("3")) {
+                m = mark.YUGO.toString();
+            } else if (option.equals("4")) {
+                m = mark.OPEL.toString();
+            }
         }
 
         return m;
     }
 
     public static String pickModel() {
-        System.out.println("\n1) Model 1");
-        System.out.println("2) Model 2");
-        System.out.println("3) Model 3");
-        System.out.println("4) Model 4");
-        System.out.print(">>> Model : ");
-        String option = scanner.nextLine();
-
+        String option = "";
         String m = "";
 
-        if (option.equals("1")) {
-            m = model.ONE.toString();
-        }
-        if (option.equals("2")) {
-            m = model.TWO.toString();
-        }
-        if (option.equals("3")) {
-            m = model.THREE.toString();
-        }
-        if (option.equals("4")) {
-            m = model.FOUR.toString();
+        while (!option.matches("[1-4]")) {
+            System.out.println("\n1) Model 1");
+            System.out.println("2) Model 2");
+            System.out.println("3) Model 3");
+            System.out.println("4) Model 4");
+            System.out.print(">>> Model : ");
+            option = scanner.nextLine();
+
+            if (option.equals("1")) {
+                m = model.ONE.toString();
+            } else if (option.equals("2")) {
+                m = model.TWO.toString();
+            } else if (option.equals("3")) {
+                m = model.THREE.toString();
+            } else if (option.equals("4")) {
+                m = model.FOUR.toString();
+            }
         }
 
         return m;
     }
 
     public static String pickFuel() {
-        System.out.println("\n1) Dizel");
-        System.out.println("2) Elektricni pogon");
-        System.out.println("3) Gas");
-        System.out.print(">>> Gorivo : ");
-        String option = scanner.nextLine();
-
+        String option = "";
         String f = "";
 
-        if (option.equals("1")) {
-            f = fuel.DIZEL.toString();
-        }
-        if (option.equals("2")) {
-            f = fuel.ELECTRIC.toString();
-        }
-        if (option.equals("3")) {
-            f = fuel.GAS.toString();
+        while (!option.matches("[1-4]")) {
+            System.out.println("\n1) Dizel");
+            System.out.println("2) Elektricni pogon");
+            System.out.println("3) Gas");
+            System.out.print(">>> Gorivo : ");
+            option = scanner.nextLine();
+
+            if (option.equals("1")) {
+                f = fuel.DIZEL.toString();
+            } else if (option.equals("2")) {
+                f = fuel.ELECTRIC.toString();
+            } else if (option.equals("3")) {
+                f = fuel.GAS.toString();
+            }
         }
 
         return f;
     }
 
     public static String pickSpecialization() {
-        System.out.println("\n1) Auto-mehanicar");
-        System.out.println("2) Auto-elektricar");
-        System.out.println("3) Vulkanizer");
-        System.out.println("4) Limar");
-        System.out.print(">>> Specijalizacija : ");
-        String option = scanner.nextLine();
-
+        String option = "";
         String s = "";
 
-        if (option.equals("1")) {
-            s = specialization.AUTOMEHANICAR.toString();
-        }
-        if (option.equals("2")) {
-            s = specialization.AUTOELEKTRICAR.toString();
-        }
-        if (option.equals("3")) {
-            s = specialization.VULKANIZER.toString();
-        }
-        if (option.equals("4")) {
-            s = specialization.LIMAR.toString();
+        while (!option.matches("[1-4]")) {
+            System.out.println("\n1) Auto-mehanicar");
+            System.out.println("2) Auto-elektricar");
+            System.out.println("3) Vulkanizer");
+            System.out.println("4) Limar");
+            System.out.print(">>> Specijalizacija : ");
+            option = scanner.nextLine();
+
+            if (option.equals("1")) {
+                s = specialization.AUTOMEHANICAR.toString();
+            } else if (option.equals("2")) {
+                s = specialization.AUTOELEKTRICAR.toString();
+            } else if (option.equals("3")) {
+                s = specialization.VULKANIZER.toString();
+            } else if (option.equals("4")) {
+                s = specialization.LIMAR.toString();
+            }
         }
 
         return s;
     }
 
     public static String pickGender() {
-        System.out.println("\n1) Muski");
-        System.out.println("2) Zenski");
-        System.out.print(">>> Pol : ");
-        String option = scanner.nextLine();
-
+        String option = "";
         String g = "";
 
-        if (option.equals("1")) {
-            g = gender.MALE.toString();
-        }
-        if (option.equals("2")) {
-            g = gender.FEMALE.toString();
+        while (!option.matches("[1-4]")) {
+            System.out.println("\n1) Muski");
+            System.out.println("2) Zenski");
+            System.out.print(">>> Pol : ");
+            option = scanner.nextLine();
+
+            if (option.equals("1")) {
+                g = gender.MALE.toString();
+            } else if (option.equals("2")) {
+                g = gender.FEMALE.toString();
+            }
         }
 
         return g;
     }
 
     public static String pickStatus() {
-        System.out.println("\n1) Gotov");
-        System.out.println("2) U toku");
-        System.out.println("3) Nije poceo");
-        System.out.print(">>> Status : ");
-        String option = scanner.nextLine();
-
+        String option = "";
         String s = "";
 
-        if (option.equals("1")) {
-            s = status.FINISHED.toString();
-        }
-        if (option.equals("2")) {
-            s = status.NOTFINISHED.toString();
-        }
-        if (option.equals("3")) {
-            s = status.NOTSTARTED.toString();
+        while (!option.matches("[1-4]")) {
+            System.out.println("\n1) Gotov");
+            System.out.println("2) U toku");
+            System.out.println("3) Nije poceo");
+            System.out.print(">>> Status : ");
+            option = scanner.nextLine();
+
+            if (option.equals("1")) {
+                s = status.FINISHED.toString();
+            } else if (option.equals("2")) {
+                s = status.NOTFINISHED.toString();
+            } else if (option.equals("3")) {
+                s = status.NOTSTARTED.toString();
+            }
         }
 
         return s;
