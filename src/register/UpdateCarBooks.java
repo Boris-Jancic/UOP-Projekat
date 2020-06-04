9f68a5fc3d331c9e1d02f8e9adcc1724a7d42d64
Boris Jancic <jancicboris56@gmail.com>
2020-06-04 18:29:27 +0200

    Carbook constructor now takes a boolean

--- src/register/UpdateCarBooks.java
+++ src/register/UpdateCarBooks.java
@@ -5,14 +5,13 @@ import carModels.Service;
 import java.util.ArrayList;
 
 public class UpdateCarBooks {
-    public static ArrayList<CarBook> update(ArrayList<Service> services, ArrayList<CarBook> carBooks) { // TODO : fix this
+    public static ArrayList<CarBook> update(ArrayList<Service> services, ArrayList<CarBook> carBooks) {
         ArrayList<CarBook> carBooksReturn = new ArrayList<>();
         ArrayList<String> carBookIDs = new ArrayList<>();
 
-
         for (CarBook carBook : carBooks) {  // Proverava da li postoji auto koji vec ima knjizicu i ako postoji upisuje servis u nju
             ArrayList<Service> servicesReturn = new ArrayList<>();
-            CarBook carBookNew = new CarBook(carBook.getCar());
+            CarBook carBookNew = new CarBook(false, carBook.getCar());
 
             for (Service service : services) {
                 if (carBookNew.getCar().getCarID().equals(service.getCar().getCarID())) {
@@ -24,14 +23,13 @@ public class UpdateCarBooks {
             carBooksReturn.add(carBookNew);
         }
 
-
         for (CarBook c : carBooks) {
             carBookIDs.add(c.getCar().getCarID());
         }
 
         for (Service service : services) {  // Ako postoji auto koji nema ni jedan servis napravice se nova knjizica
             if (!carBookIDs.contains(service.getCar().getCarID())) {
-                CarBook carBook = new CarBook(service.getCar());
+                CarBook carBook = new CarBook(false, service.getCar());
                 carBook.addService(service);
                 carBooksReturn.add(carBook);
                 break;
