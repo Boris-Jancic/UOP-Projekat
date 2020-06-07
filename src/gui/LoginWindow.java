e811f7c39297eed935c1e451c3878ebcde1e050b
Boris Jancic <jancicboris56@gmail.com>
2020-06-07 21:57:22 +0200

    Login window pop-up added

--- src/gui/LoginWindow.java
+++ src/gui/LoginWindow.java
@@ -1,9 +1,17 @@
 package gui;
 
 import net.miginfocom.swing.MigLayout;
-import javax.swing.*;
+import userModels.Client;
+import userModels.Person;
+import utility.Checks;
 
-public class login extends JFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Set;
+
+public class LoginWindow extends JFrame {
     private JLabel lblWelcome = new JLabel("Dobrodosli. Molimo da se prijavite.");
     private JLabel lblUsername = new JLabel("Korisnicko ime");
     private JTextField txtUsername = new JTextField(20);
@@ -13,13 +21,18 @@ public class login extends JFrame {
     private JButton btnOk = new JButton("Uloguj se");
     private JButton btnCancel = new JButton("Otkazi");
 
-    public login() {
+    private Set<Person> people;
+
+    public LoginWindow(Set<Person> people) {
+        this.people = people;
         setTitle("Prijava korisnika");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         initGUI();
         initActions();
         pack();
+
+        getRootPane().setDefaultButton(btnOk);
     }
 
     private void initGUI() {
@@ -37,6 +50,39 @@ public class login extends JFrame {
     }
 
     private void initActions() {
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LoginWindow.this.dispose();
+                LoginWindow.this.setVisible(false);
+            }
+        });
 
-    }
+        btnOk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = txtUsername.getText().trim();
+                String password = new String(pfPassword.getPassword()).trim();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Niste uneli sve podatke",
+                                                    "Greska", JOptionPane.WARNING_MESSAGE);
+                } else {
+                    Person person = Checks.Login(people, username, password);
+
+                    if (person == null) {
+                        JOptionPane.showMessageDialog(null, "Nevazeca sifra ili korisniko ime",
+                                "Greska", JOptionPane.WARNING_MESSAGE);
+                    }
+
+                    if (person instanceof Client) {
+                        LoginWindow.this.dispose();
+                        LoginWindow.this.setVisible(false);
+                        System.out.println(person);
+                        ClientWindow clientWindow = new ClientWindow((Client) person);
+                        clientWindow.setVisible(true);
+                    }
+                }
+            }
+        });}
 }
