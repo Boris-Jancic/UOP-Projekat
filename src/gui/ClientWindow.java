97f6e9cab7383faa9094bd4d1cbd2857672537e4
Boris Jancic <jancicboris56@gmail.com>
2020-06-12 00:11:17 +0200

    More client data is displayed

--- src/gui/ClientWindow.java
+++ src/gui/ClientWindow.java
@@ -1,11 +1,12 @@
 package gui;
 
+import enums.Gender;
 import guiTables.CarTable;
 import guiTables.ServiceTable;
 import main.Access;
+import net.miginfocom.swing.MigLayout;
 import userModels.Client;
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,18 +16,24 @@ public class ClientWindow extends JFrame {
     private JMenuItem carItem = new JMenuItem("Manipulisanje");
     private JMenuItem serviceItem = new JMenuItem("Servisi");
 
+
+    private JLabel lblName;
+    private JLabel lblLastName;
+    private JLabel lblJmbg;
+    private JLabel lblPhone;
+    private JLabel lblAddress;
+    private JLabel lblUserName;
+    JLabel lblMalePicture = new JLabel(new ImageIcon("src/pictures/client-male.png"));
+    JLabel lblFemalePicture = new JLabel(new ImageIcon("src/pictures/client-female.png"));
+
     private Access access;
     private Client client;
-
-    public DefaultTableModel tableModel;
-    private JTable carTable;
-    private JTable serviceTable;
 
     public ClientWindow(Access access, Client client) {
         this.access = access;
         this.client = client;
         setTitle("Musterija : " + client.getName());
-        setSize(500,500);
+        setSize(380,380);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         initMenu();
@@ -38,6 +45,22 @@ public class ClientWindow extends JFrame {
         mainMenu.add(carMenu);
         carMenu.add(carItem);
         mainMenu.add(serviceItem);
+
+
+        MigLayout migLayout = new MigLayout("wrap 1", "10[][]");
+        setLayout(migLayout);
+
+        if(client.getGender() == Gender.MALE) {
+            add(lblMalePicture, "gapleft 80");
+        } else {
+            add(lblFemalePicture, "gapleft 80");
+        }
+        add(lblName = new JLabel("Ime : " + client.getName()), "gapleft 90");
+        add(lblLastName = new JLabel("Prezime : " + client.getLastName()),"gapleft 90");
+        add(lblJmbg = new JLabel("JMBG : " + client.getJmbg()), "gapleft 90");
+        add(lblPhone = new JLabel("Telefon : " + client.getPhone()), "gapleft 90");
+        add(lblAddress = new JLabel("Adresa : " + client.getAddress()), "gapleft 90");
+        add(lblUserName = new JLabel("Korisnicko ime : " + client.getUsername()), "gapleft 90");
     }
 
     private void initActions () {
@@ -51,7 +74,7 @@ public class ClientWindow extends JFrame {
         serviceItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                ServiceTable serviceWindow = new ServiceTable(access, 0);
+                ServiceTable serviceWindow = new ServiceTable(access, client);
                 serviceWindow.setVisible(true);
             }
         });
