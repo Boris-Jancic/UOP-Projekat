654ede5c292f536e9c557ec2e9462d957f49f1bd
Boris Jancic <jancicboris56@gmail.com>
2020-06-12 19:02:18 +0200

    Added check for client requests

--- src/main/LoadServices.java
+++ src/main/LoadServices.java
@@ -18,22 +18,32 @@ public class LoadServices {
         String[] servicesString = ReadFromFile.read("src/data/services.txt").split("\n");
         ArrayList<Service> Services = new ArrayList<>();
 
-        for (String service : servicesString) {
-            if (!service.isEmpty()) {
-                String[] serviceSplit = service.split("\\|");
-
+        for (String serviceStr : servicesString) {
+            Service service = null;
+            if (!serviceStr.isEmpty()) {
+                String[] serviceSplit = serviceStr.split("\\|");
                 Car car = Checks.findCar(serviceSplit[0], cars);
 
-                Worker worker = Checks.findWorker(serviceSplit[1], people);
-                GregorianCalendar date = Checks.stringToDate(serviceSplit[2]);
-                String description = serviceSplit[3];
-                ArrayList<Part> usedParts = Checks.findParts(serviceSplit[4].split(";"), parts);
-                Status state = Status.valueOf(serviceSplit[5]);
-                String serviceID = serviceSplit[6];
-                boolean deleted = Boolean.parseBoolean(serviceSplit[7]);
 
-                Service ser = new Service(car, worker, date, description, usedParts, state, serviceID, deleted);
-                Services.add(ser);
+                if (serviceSplit.length == 5) {
+                    String description = serviceSplit[1];
+                    Status state = Status.valueOf(serviceSplit[2]);
+                    String serviceID = serviceSplit[3];
+                    boolean deleted = Boolean.parseBoolean(serviceSplit[4]);
+                    service = new Service(car, description, state, serviceID, deleted);
+
+                } else {
+                    Worker worker = Checks.findWorker(serviceSplit[1], people);
+                    GregorianCalendar date = Checks.stringToDate(serviceSplit[2]);
+                    String description = serviceSplit[3];
+                    ArrayList<Part> usedParts = Checks.findParts(serviceSplit[4].split(";"), parts);
+                    Status state = Status.valueOf(serviceSplit[5]);
+                    String serviceID = serviceSplit[6];
+                    boolean deleted = Boolean.parseBoolean(serviceSplit[7]);
+                    service = new Service(car, worker, date, description, usedParts, state, serviceID, deleted);
+                }
+
+                Services.add(service);
             }
         }
 
